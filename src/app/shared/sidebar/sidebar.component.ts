6b473861a181d4000796de3c6809c432368bf9a5
jeffssant <santana.jeff@gmail.com>
2021-04-20 19:37:43 -0300

    inicializando App

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',  
 })
-export class SidebarComponent implements OnInit {
 
-  constructor() { }
+export class SidebarComponent  
+{
 
-  ngOnInit(): void {
+  get historial()
+  {
+    return this.gifsservice.historial;
   }
+
+  constructor(private gifsservice: GifsService) { }
 
 }
